6ff6db93c840294bcf2059f5f23ff33f2589e9c0
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-05-24 15:38:45 -0300

    Java Streams examples forEach and toList()

--- src/Estudante.java
+++ src/Estudante.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Estudante {
+public class Estudante {
+
+  private String matricula;
+  private String nome;
+  private String curso;
+
+  public Estudante(String matricula, String nome, String curso) {
+    this.matricula = matricula;
+    this.nome = nome;
+    this.curso = curso;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getMatricula() {
+    return matricula;
+  }
+
+  public void setMatricula() {
+    this.matricula = matricula;
+  }
+
+  public String getCurso() {
+    return curso;
+  }
+
+  public void setCurso() {
+    this.curso = curso;
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+  public static Collection<Estudante> obterEntrada() {
+    return Set.of(
+        new Estudante("2001", "Maria", "Computação"),
+        new Estudante("2002", "João", "Computação"),
+        new Estudante("2003", "José", "Pedagogia"),
+        new Estudante("2004", "Ana", "Computação"),
+        new Estudante("2005", "Bernardo", "Engenharia"),
+        new Estudante("2006", "Antônia", "Computação"));
+  }
+  public static void main(String[] args) {
+    // usando Stream com forEach para retornar um valor fora de uma lista
+    Collection<Estudante> entrada = obterEntrada();
+    System.out.println("Estudantes de computação: ");
+    entrada.stream()
+        .filter(e -> "Computação".equals(e.getCurso()))
+        .sorted(Comparator.comparing(Estudante::getNome))
+        .map(Estudante::getNome)
+        .forEach(System.out::println);
+
+    List<String> matriculas = entrada.stream()
+        .filter(e -> "Computação".equals(e.getCurso()))
+        .sorted(Comparator.comparing((Estudante::getNome)))
+        .map(Estudante::getMatricula)
+        .toList();
+    System.out.println("Matriculas: "+ matriculas);
+  }
 }
